4af0fc518461ec6c8763c788ec800982a9b7d18d
eric-klimowich <eklimowich@hotmail.com>
2019-08-08 13:32:04 -0400

    Adding and using npm modules to app

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-const add = require('./utils.js')
+const chalk = require('chalk')
+const validator = require('validator')
+const getNotes = require('./notes.js')
+// const add = require('./utils.js')
 // const name = require('./utils.js')
 // require('./utils.js')
 
 // const name = 'Eric'
 
-const sum = add(4, -2)
+// const sum = add(4, -2)
 
-console.log(sum)
+const notes = getNotes()
+
+console.log(chalk.green.inverse.bold('Success!'))
+console.log(validator.isEmail('eric@example.com'))
+console.log(validator.isEmail('example.com'))
+console.log(notes)
+// console.log(sum)
 // console.log(name)
 
 
