ed5e2fdef33e69822adb61b8fe9fd88dca6ab717
eric-klimowich <eklimowich@hotmail.com>
2019-08-08 14:29:48 -0400

    Using yargs to set up commands to perform various functions for our note app

--- app.js
+++ app.js
@@ -2,18 +2,64 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const notes = getNotes()
 
-console.log(chalk.green.inverse.bold('Success!'))
-console.log(notes)
+// Customize yargs version
+yargs.version('1.1.0')
 
-console.log(process.argv)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        console.log('Removing the note!')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function() {
+        console.log('Reading a note!')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function() {
+        console.log('Listing out all notes!')
+    }
+})
+
+// add, remove, read, list
+
+
 console.log(yargs.argv)
 
-const command = process.argv[2]
 
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// const notes = getNotes()
+
+// console.log(chalk.green.inverse.bold('Success!'))
+// console.log(notes)
+
+// console.log(process.argv)
+// console.log(yargs.argv)
+
+// const command = process.argv[2]
+
+// if (command === 'add') {
+//     console.log('Adding note!')
+// } else if (command === 'remove') {
+//     console.log('Removing note!')
+// }
