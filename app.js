284376ea02ec3c5b44fa57e58e4bb23bece75e6b
eric-klimowich <eklimowich@hotmail.com>
2019-08-08 11:47:17 -0400

    Start of notes app using Node.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const name = require('./utils.js')
+// require('./utils.js')
+
+// const name = 'Eric'
+
+console.log(name)
+
+
+// const fs = require('fs')
+
+// fs.writeFileSync('notes.txt', 'This file was created by Node.js!')
+// fs.writeFileSync('notes.txt', 'My name is Eric.')
+
+// fs.appendFileSync('notes.txt', ' I now live in Cortlandt Manor, NY.')
