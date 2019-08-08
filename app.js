3b631f42f647e012b77b7cf4bc34fe9acd785c2f
eric-klimowich <eklimowich@hotmail.com>
2019-08-08 13:46:05 -0400

    Removed sample code to practice adding modules, imports, functions, etc.

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
 const chalk = require('chalk')
-const validator = require('validator')
 const getNotes = require('./notes.js')
-// const add = require('./utils.js')
-// const name = require('./utils.js')
-// require('./utils.js')
-
-// const name = 'Eric'
-
-// const sum = add(4, -2)
 
 const notes = getNotes()
 
 console.log(chalk.green.inverse.bold('Success!'))
-// console.log(validator.isEmail('eric@example.com'))
-// console.log(validator.isEmail('example.com'))
+
 console.log(notes)
-// console.log(sum)
-// console.log(name)
 
 
 // const fs = require('fs')
